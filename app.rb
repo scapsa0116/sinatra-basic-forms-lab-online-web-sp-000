011595e9b447da4bde85918d55bad23aa77632a4
Stela Capsa <stela.capsa@gmail.com>
2020-03-31 19:17:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require_relative 'config/environment'
-
+require_relative 'models/puppy.rb'
 class App < Sinatra::Base
+  
+    get '/' do 
+     erb :create_puppy
+    end 
 
+# get '/create_puppy' do 
+#   erb :create_puppy
+# end 
+
+get '/puppy'
+erb :display_puppy
+end 
+
+   post '/puppy' do 
+     @puppy =Puppy.new{params[:name]
+     erb :display_puppy
+   end 
+ 
 end
