9af6e226b847cfcb9368a8d1ed145adfa1fe72f2
Stela Capsa <stela.capsa@gmail.com>
2020-03-31 19:19:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,9 +10,9 @@ class App < Sinatra::Base
 #   erb :create_puppy
 # end 
 
-# get '/puppy'
-# erb :display_puppy
-# end 
+get '/puppy'
+erb :display_puppy
+end 
 
   post '/puppy' do 
      @puppy =Puppy.new
